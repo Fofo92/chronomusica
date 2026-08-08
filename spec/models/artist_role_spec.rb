dfb301737c0b1796265350ac131768c2ab2afac2
Pascal Fodiman <pascal@fodiman.fr>
2026-08-08 02:08:09 +0200

    Add the Person–ArtistRole relationship

--- spec/models/artist_role_spec.rb
+++ spec/models/artist_role_spec.rb
@@ -27,4 +27,26 @@ RSpec.describe ArtistRole, type: :model do
       expect(role.people).to contain_exactly(bach, mozart)
     end
   end
+
+  describe "validations" do
+    it "requires a name" do
+      role = described_class.new(name: nil)
+
+      expect(role).not_to be_valid
+      expect(role.errors[:name]).to include("can't be blank")
+    end
+
+    it "requires a unique name" do
+      described_class.create!(
+        name: "composer"
+      )
+
+      duplicate = described_class.new(
+        name: "composer"
+      )
+
+      expect(duplicate).not_to be_valid
+      expect(duplicate.errors[:name]).to include("has already been taken")
+    end
+  end
 end
